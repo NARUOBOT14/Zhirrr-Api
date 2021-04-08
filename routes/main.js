f5a797e6ecd345cb05e2e8f46fa5b722ebabbb4f
NARUOBOT14 <78155011+NARUOBOT14@users.noreply.github.com>
2021-04-08 22:33:55 +0700

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -24,9 +24,9 @@ router.get('/config', (req, res) => {
         status: true,
         result: {
             prefix : '/',
-            namabot: 'ZhirrrBot',
-            namaowner: 'Zhirrr',
-            instagram: 'zhirr_ajalah',
+            namabot: 'RizkyBot',
+            namaowner: 'Rizky',
+            instagram: 'riski.pfp',
             youtube : 'Gak Punya'
         }
     }
